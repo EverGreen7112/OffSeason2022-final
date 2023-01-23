65fa4d67c70ad3df364c35e4727fff4b47cd7cad
itayst1 <itay.steinberg2007@gmail.com>
2023-01-23 18:52:30 +0200

    talon srx internal pid

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import com.ctre.phoenix.CANifier.PWMChannel;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -12,7 +14,7 @@ import frc.robot.EverLibEssentials.Motors;
 import frc.robot.statics.Autonomus;
 import frc.robot.statics.Constants;
 import frc.robot.statics.Controls;
-
+import edu.wpi.first.wpilibj.I2C;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -28,12 +30,15 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+  public static AHRS navX;
   @Override
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     //Constants.UsableMotors.init();
     m_robotContainer = new RobotContainer();
+    navX = new AHRS(I2C.Port.kMXP);
+    Constants.UsableMotors.FLY_WHEEL.setSelectedSensorPosition(0);
   }
 
   /**
@@ -43,13 +48,57 @@ public class Robot extends TimedRobot {
    * <p>This runs after the mode specific periodic functions, but before LiveWindow and
    * SmartDashboard integrated updating.
    */
-  @Override
+  public static long lastTime = System.currentTimeMillis();
+  public static double velocityX = 0;
+  public static double x = 0;
+  public static double velocityY = 0;
+  public static double y = 0;
+  public static double velocityZ = 0;
+  public static double z = 0;
+   @Override
   public void robotPeriodic() {
     //SmartDashboard.putNumber("position: ", Constants.UsableMotors.FLY_WHEEL.getSelectedSensorPosition());
     double angle = Controls.m_hubVision.getAngleX();
-    //SmartDashboard.putNumber("angle2", angle);
+    SmartDashboard.putNumber("yaw", navX.getYaw());
+    SmartDashboard.putNumber("pitch", navX.getPitch());
+    SmartDashboard.putNumber("roll", navX.getRoll());
+    SmartDashboard.putNumber("angle", navX.getAngle());
+    SmartDashboard.putNumber("accel",navX.getWorldLinearAccelZ() * 9.806);
 
-  }
+    long time = System.currentTimeMillis();
+    double deltaTime = ((time - lastTime) / 1000.0);
+    SmartDashboard.putNumber("delta",deltaTime);
+    lastTime = time;
+    
+    double accelerationZ = (navX.getWorldLinearAccelZ() * 9.806);
+    double accelerationX = (navX.getWorldLinearAccelX() * 9.806);
+    double accelerationY = (navX.getWorldLinearAccelY() * 9.806);
+    
+    double deltaVelocityX = accelerationX * deltaTime; 
+    double deltaX = velocityX * deltaTime + 0.5 * (accelerationX * deltaTime * deltaTime);
+    velocityX += deltaVelocityX;
+    x += deltaX;
+
+    double deltaVelocityY = accelerationY * deltaTime; 
+    double deltaY = velocityY * deltaTime + 0.5 * (accelerationY * deltaTime * deltaTime);
+    velocityY += deltaVelocityY;
+    y += deltaY;
+
+    double deltaVelocityZ = accelerationZ * deltaTime; 
+    double deltaZ = velocityZ * deltaTime + 0.5 * (accelerationZ * deltaTime * deltaTime);
+    velocityZ += deltaVelocityZ;
+    z += deltaZ;
+       
+    SmartDashboard.putNumber("x", x);
+    SmartDashboard.putNumber("y", y);
+    SmartDashboard.putNumber("z", z);
+    SmartDashboard.putNumber("vx",velocityX);
+    SmartDashboard.putNumber("vy",velocityY);
+    SmartDashboard.putNumber("vz",velocityZ);
+
+}
+
+    //SmartDashboard.putNumber("angle2", angle);
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
@@ -97,20 +146,28 @@ public class Robot extends TimedRobot {
     Motors.runMotors();
     SmartDashboard.putNumber("ticks", Constants.UsableMotors.FLY_WHEEL.getSelectedSensorPosition());
   }
-
+  double target;
   @Override
   public void testInit() {
     //Motors.deleteRecordingsByOrder(300);
     ////SmartDashboard.putString("motor", PWMChannel.valueOf(1).name());
-
-
+    // Controls.navX.reset();
+    Constants.UsableMotors.FLY_WHEEL.selectProfileSlot(0, 0);
+    Constants.UsableMotors.FLY_WHEEL.config_kP(0, 0.006 * 1.6);
+    Constants.UsableMotors.FLY_WHEEL.config_kI(0, 0.000003);
+    Constants.UsableMotors.FLY_WHEEL.config_kD(0, 0.000005);
+    Constants.UsableMotors.FLY_WHEEL.setSelectedSensorPosition(0);
+    // Constants.UsableMotors.FLY_WHEEL.setInverted(true);
+    // Constants.UsableMotors.FLY_WHEEL.set(TalonSRXControlMode.Position, 8196);
   }
 
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {
-
-    
+    Constants.UsableMotors.FLY_WHEEL.setSensorPhase(true);
+    Constants.UsableMotors.FLY_WHEEL.set(TalonSRXControlMode.Position, 8196*2);
+    SmartDashboard.putNumber("target", Constants.UsableMotors.FLY_WHEEL.getClosedLoopTarget());
+    SmartDashboard.putNumber("error", Constants.UsableMotors.FLY_WHEEL.getClosedLoopError());
   }
 
   /** This function is called once when the robot is first started up. */
--- src/main/java/frc/robot/statics/Controls.java
+++ src/main/java/frc/robot/statics/Controls.java
@@ -3,12 +3,16 @@ package frc.robot.statics;
 import java.util.LinkedList;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SPI;
+
 import edu.wpi.first.wpilibj.drive.Vector2d;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,35 +23,37 @@ import frc.robot.EverLibEssentials.Motor;
 import frc.robot.EverLibEssentials.Motors;
 
 public class Controls {
-    private static Joystick m_rightJoystick  = new Joystick(Constants.JoystickPorts.rightJoystick), m_leftJoystick, m_operator; 
-	private static JoystickButton m_cancelAll;
+  private static Joystick m_rightJoystick = new Joystick(Constants.JoystickPorts.rightJoystick), m_leftJoystick,
+      m_operator;
+  private static JoystickButton m_cancelAll;
   private static Trigger m_fire;
-  private static JoystickButton m_collectorCollect, m_collectorUncollect, m_collectorOpen, m_collectorClose, m_climberDown,
-   m_climberUp, m_storageUp, m_storageDown, m_load, m_turnToShoot,m_driveStright, m_turbu = new JoystickButton(m_rightJoystick, 1);
+  private static JoystickButton m_collectorCollect, m_collectorUncollect, m_collectorOpen, m_collectorClose,
+      m_climberDown,
+      m_climberUp, m_storageUp, m_storageDown, m_load, m_turnToShoot, m_driveStright,
+      m_turbu = new JoystickButton(m_rightJoystick, 1);
 
-   public static Vision m_hubVision = new Vision(Constants.ComPorts.hubVision);
-   public static PIDMotor m_shootPidMotor = new PIDMotor(Constants.UsableMotors.FLY_WHEEL,
-     Constants.PIDValues.FLY_WHEEL_KP,
-     Constants.PIDValues.FLY_WHEEL_KI,
-     Constants.PIDValues.FLY_WHEEL_KD);
-     
-    
+  public static Vision m_hubVision = new Vision(Constants.ComPorts.hubVision);
+  public static PIDMotor m_shootPidMotor = new PIDMotor(Constants.UsableMotors.FLY_WHEEL,
+      Constants.PIDValues.FLY_WHEEL_KP,
+      Constants.PIDValues.FLY_WHEEL_KI,
+      Constants.PIDValues.FLY_WHEEL_KD);
+
+      
 
   public static void init() {
-  
+
     // initialize joysticks and operator
-    // m_rightJoystick 
+    // m_rightJoystick
     m_leftJoystick = new Joystick(Constants.JoystickPorts.leftJoystick);
     m_operator = new Joystick(Constants.JoystickPorts.operator);
     m_driveStright = new JoystickButton(m_rightJoystick, 4);
     // m_turbo = new JoystickButton(m_rightJoystick, Constants.ButtonPorts.turbo);
-	  // m_cancelAll =   new JoystickButton(m_rightJoystick,5);
+    // m_cancelAll = new JoystickButton(m_rightJoystick,5);
     // initialize buttons for specific commands later on
     m_collectorOpen = new JoystickButton(m_operator, Constants.ButtonPorts.collectorOpen);
     m_collectorClose = new JoystickButton(m_operator, Constants.ButtonPorts.collectorClose);
     m_collectorCollect = new JoystickButton(m_operator, Constants.ButtonPorts.collectorCollect);
     m_collectorUncollect = new JoystickButton(m_operator, Constants.ButtonPorts.collectorUncollect);
-
 
     m_climberDown = new JoystickButton(m_operator, Constants.ButtonPorts.climberDown);
     m_climberUp = new JoystickButton(m_operator, Constants.ButtonPorts.climberUp);
@@ -55,11 +61,11 @@ public class Controls {
     m_storageUp = new JoystickButton(m_operator, Constants.ButtonPorts.storageUp);
     m_storageDown = new JoystickButton(m_operator, Constants.ButtonPorts.storageDown);
 
-    m_turnToShoot =  new JoystickButton(m_rightJoystick, Constants.ButtonPorts.turnToShoot);
+    m_turnToShoot = new JoystickButton(m_rightJoystick, Constants.ButtonPorts.turnToShoot);
 
     m_load = new JoystickButton(m_operator, Constants.ButtonPorts.load);
     m_fire = new POVButton(m_operator, 0);
-    for (int i =0; i <= 360; i++){
+    for (int i = 0; i <= 360; i++) {
       m_fire = m_fire.or(new POVButton(m_operator, i));
     }
     turnPID.setSetpoint(0);
@@ -67,131 +73,133 @@ public class Controls {
     Constants.UsableMotors.COLLECTOR_COLLECT.setInverted(true);
     Constants.UsableMotors.BOTTOM_STORAGE.setInverted(true);
     Constants.UsableMotors.STORAGE_TOP.setInverted(true);
+    // Constants.UsableMotors.FLY_WHEEL.setInverted(true);
     Constants.UsableMotors.FLY_WHEEL.configFactoryDefault();
-    Constants.UsableMotors.FLY_WHEEL.setSensorPhase(false);
+    Constants.UsableMotors.FLY_WHEEL.setSensorPhase(true);
     // initialize commands on buttons
 
   }
   // public static JoystickButton getM_turbu() {
-  //     return m_turbu;
+  // return m_turbu;
   // }
 
   public static void driveByJoysticks() {
     Vector2d speedVector = new Vector2d(m_rightJoystick.getY() * Constants.Speeds.moveSpeed,
-     m_leftJoystick.getY() * Constants.Speeds.moveSpeed);
-     double magnitude = speedVector.magnitude();
-    if ( magnitude > Constants.Speeds.speedLimit){
-      speedVector.x = (speedVector.x / magnitude) *  Constants.Speeds.speedLimit;
-      speedVector.y = (speedVector.y / magnitude) *  Constants.Speeds.speedLimit;
+        m_leftJoystick.getY() * Constants.Speeds.moveSpeed);
+    double magnitude = speedVector.magnitude();
+    if (magnitude > Constants.Speeds.speedLimit) {
+      speedVector.x = (speedVector.x / magnitude) * Constants.Speeds.speedLimit;
+      speedVector.y = (speedVector.y / magnitude) * Constants.Speeds.speedLimit;
     }
-    if(!m_turbu.get()){ 
-    tankDrive(speedVector.x, speedVector.y);
-    }
-    else{
-      tankDrive(m_rightJoystick.getY(),  m_leftJoystick.getY() );
+    if (!m_turbu.get()) {
+      tankDrive(speedVector.x, speedVector.y);
+    } else {
+      tankDrive(m_rightJoystick.getY(), m_leftJoystick.getY());
     }
   }
-  
-  public static void tankDrive(double speedRight, double speedLeft){
+
+  public static void tankDrive(double speedRight, double speedLeft) {
     Constants.UsableMotors.CHASSIS_RIGHT.set(speedRight);
     Constants.UsableMotors.CHASSIS_LEFT.set(speedLeft);
   }
+
   public static boolean finished2 = false;
-  public static void driveStright(){
-  
+
+  public static void driveStright() {
+
     double dist = m_hubVision.getZ();
     b.add(dist);
-    // if(b.size() > 5){ 
+    // if(b.size() > 5){
     // double myAng = b.getLast() + b.get(b.size()-2) + b.get(b.size() -3);
     // dist = myAng/3;
     // }
 
     // double speed = turnPID.calculate(angle,0);
-    //SmartDashboard.putNumber(*"turnSpeed", speed);
-    //SmartDashboard.putNumber("angle", angle);
-    if (!finished2 && Math.abs(dist - 5.9)>0.4) {
-      tankDrive(-Math.signum(dist-5.9)*0.23,-Math.signum(dist-5.9)* 0.23);
-    }
-    else{
-     tankDrive(0,0);
+    // SmartDashboard.putNumber(*"turnSpeed", speed);
+    // SmartDashboard.putNumber("angle", angle);
+    if (!finished2 && Math.abs(dist - 5.9) > 0.4) {
+      tankDrive(-Math.signum(dist - 5.9) * 0.23, -Math.signum(dist - 5.9) * 0.23);
+    } else {
+      tankDrive(0, 0);
       b = new LinkedList<Double>();
       finished2 = true;
+    }
+    // float distance = m_hubVision.getZ();
+    // speed = calcTrajectory.calcSpeed(distance,
+    // Constants.PhysicalConsts.SHOOT_HEIGHT,
+    // Constants.PhysicalConsts.SHOOT_ANGLE);
+    // m_shootPidMotor.set(speed * Constants.Speeds.SHOOT);
   }
-  // float distance = m_hubVision.getZ();
-  // speed = calcTrajectory.calcSpeed(distance, Constants.PhysicalConsts.SHOOT_HEIGHT,
-  //  Constants.PhysicalConsts.SHOOT_ANGLE);
-  //  m_shootPidMotor.set(speed * Constants.Speeds.SHOOT);
-}
-public static void unDriveStright(){
-  finished2 = false;
-  
-}
 
-private static PIDController turnPID = new PIDController(Constants.PIDValues.TURN_KP,
-   Constants.PIDValues.TURN_KI, Constants.PIDValues.TURN_KD);
+  public static void unDriveStright() {
+    finished2 = false;
 
-   private static PIDController movePID = new PIDController(0.0001,
-   0, 0);
+  }
+
+  private static PIDController turnPID = new PIDController(Constants.PIDValues.TURN_KP,
+      Constants.PIDValues.TURN_KI, Constants.PIDValues.TURN_KD);
+
+  private static PIDController movePID = new PIDController(0.0001,
+      0, 0);
   public static LinkedList<Double> a = new LinkedList<Double>();
   public static LinkedList<Double> b = new LinkedList<Double>();
 
   public static boolean finished = false;
 
-  public static void turnToShoot(){
-    
+  public static void turnToShoot() {
+
     double angle = m_hubVision.getAngleX();
     a.add(angle);
-    if(a.size() > 5){ 
-    double myAng = a.getLast() + a.get(a.size()-2) + a.get(a.size() -3);
-    angle = myAng/3;
+    if (a.size() > 5) {
+      double myAng = a.getLast() + a.get(a.size() - 2) + a.get(a.size() - 3);
+      angle = myAng / 3;
     }
 
     // double speed = turnPID.calculate(angle,0);
-    //SmartDashboard.putNumber(*"turnSpeed", speed);
-    //SmartDashboard.putNumber("angle", angle);
-    if (!finished && Math.abs(angle)>3) {
-      tankDrive(-Math.signum(angle)*0.2, Math.signum(angle)*0.2);
-    }
-    else{
-      tankDrive(0,0);
+    // SmartDashboard.putNumber(*"turnSpeed", speed);
+    // SmartDashboard.putNumber("angle", angle);
+    if (!finished && Math.abs(angle) > 3) {
+      tankDrive(-Math.signum(angle) * 0.2, Math.signum(angle) * 0.2);
+    } else {
+      tankDrive(0, 0);
       a = new LinkedList<Double>();
       finished = true;
     }
     // float distance = m_hubVision.getZ();
-    // speed = calcTrajectory.calcSpeed(distance, Constants.PhysicalConsts.SHOOT_HEIGHT,
-    //  Constants.PhysicalConsts.SHOOT_ANGLE);
-    //  m_shootPidMotor.set(speed * Constants.Speeds.SHOOT);
+    // speed = calcTrajectory.calcSpeed(distance,
+    // Constants.PhysicalConsts.SHOOT_HEIGHT,
+    // Constants.PhysicalConsts.SHOOT_ANGLE);
+    // m_shootPidMotor.set(speed * Constants.Speeds.SHOOT);
   }
-  public static void PIDturnToShoot(){
+
+  public static void PIDturnToShoot() {
     double angle = m_hubVision.getAngleX();
-    double speed = turnPID.calculate(angle,0);
+    double speed = turnPID.calculate(angle, 0);
     SmartDashboard.putNumber("turnSpeed", speed);
     SmartDashboard.putNumber("angle", angle);
   }
-  public static void unTurnToShoot(){
+
+  public static void unTurnToShoot() {
     turnPID.reset();
     finished = false;
   }
-  
+
   public static void shoot() {
-    float distance = m_hubVision.getZ();
-    // double target = calcTrajectory.calcSpeed(distance, Constants.PhysicalConsts.SHOOT_HEIGHT,
-    // Constants.PhysicalConsts.SHOOT_ANGLE) / 2;
-    m_shootPidMotor.setTarget(SmartDashboard.getNumber("target", 0));
-    // SmartDashboard.putNumber("target", );
-    m_shootPidMotor.runMotor();
+    // float distance = m_hubVision.getZ();
+    // m_shootPidMotor.setTarget(SmartDashboard.getNumber("target", 0));
+    // m_shootPidMotor.runMotor();
+    // Constants.UsableMotors.FLY_WHEEL.set(TalonSRXControlMode.Position, Constants.UsableMotors.FLY_WHEEL.getSelectedSensorPosition() + Constants.Values.TICKS_PER_REVOLUTIONS * 3);
   }
 
   public static void shootAuto() {
     float distance = m_hubVision.getZ();
-    // double speed = calcTrajectory.calcSpeed(distance, Constants.PhysicalConsts.SHOOT_HEIGHT,
+    // double speed = calcTrajectory.calcSpeed(distance,
+    // Constants.PhysicalConsts.SHOOT_HEIGHT,
     // Constants.PhysicalConsts.SHOOT_ANGLE);
     // m_shootPidMotor.set(speed * Constants.Speeds.SHOOT);
     // m_shootPidMotor.runMotor();
     Constants.UsableMotors.FLY_WHEEL.set(ControlMode.PercentOutput, 0.69 * 0.87);
   }
-  
-  
 
   public static void movePeriodic() {
 
@@ -201,55 +209,51 @@ private static PIDController turnPID = new PIDController(Constants.PIDValues.TUR
 
     // collector open
     MotorController collectorOpen = Constants.UsableMotors.COLLECTOR_OPEN;
-    //SmartDashboard.putBoolean("y button:", m_collectorOpen.get());
+    // SmartDashboard.putBoolean("y button:", m_collectorOpen.get());
     double speed = ((m_collectorOpen.get() ? 1 : 0) - (m_collectorClose.get() ? 1 : 0))
-     * Constants.Speeds.collectorOpen;
+        * Constants.Speeds.collectorOpen;
     collectorOpen.set(speed);
 
     // collector collect
     MotorController collectorCollect = Constants.UsableMotors.COLLECTOR_COLLECT;
     speed = ((m_collectorCollect.get() ? 1 : 0) - (m_collectorUncollect.get() ? 1 : 0))
-    * Constants.Speeds.collectorCollect;
+        * Constants.Speeds.collectorCollect;
     collectorCollect.set(speed);
 
     // storage
     MotorController bottomStorage = Constants.UsableMotors.BOTTOM_STORAGE;
     speed = ((m_storageUp.get() ? 1 : 0) - (m_storageDown.get() ? 1 : 0)) * Constants.Speeds.storageMotor;
     bottomStorage.set(speed);
-    
-    
+
     // shoot
-    if (m_turnToShoot.get()){
+    if (m_turnToShoot.get()) {
       turnToShoot();
-    }else{
+    } else {
       unTurnToShoot();
     }
-    if (m_load.get()){
+    if (m_load.get()) {
       Constants.UsableMotors.STORAGE_TOP.set(Constants.Speeds.storageMotor);
-    }
-    else{
+    } else {
       Constants.UsableMotors.STORAGE_TOP.set(0);
     }
-    //SmartDashboard.putBoolean("shoot: ", m_fire.get());
-    if (m_fire.get()){
+    // SmartDashboard.putBoolean("shoot: ", m_fire.get());
+    if (m_fire.get()) {
       shoot();
-    }
-    else{
-      m_shootPidMotor.stop();
-      m_shootPidMotor.reset();
+    } else {
+      // m_shootPidMotor.stop();
+      // m_shootPidMotor.reset();
       Constants.UsableMotors.STORAGE_TOP.set(Constants.UsableMotors.STORAGE_TOP.get() +
-        (m_storageDown.get() ? -1 : 0) * Constants.Speeds.storageMotor);
+          (m_storageDown.get() ? -1 : 0) * Constants.Speeds.storageMotor);
     }
-    if(m_driveStright.get()){
+    if (m_driveStright.get()) {
       driveStright();
-    }else{
+    } else {
       unDriveStright();
     }
 
-    
-    
     // Climb
-    Constants.UsableMotors.CLIMBER_MOTOR.set(((m_climberUp.get() ? 1 : 0) + (m_climberDown.get() ? -1 : 0)) * Constants.Speeds.climberMotor);
-  
+    Constants.UsableMotors.CLIMBER_MOTOR
+        .set(((m_climberUp.get() ? 1 : 0) + (m_climberDown.get() ? -1 : 0)) * Constants.Speeds.climberMotor);
+
   }
 }
